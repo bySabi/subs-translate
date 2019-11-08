4c3e5767e57b21756e84ea9b0e8e9024bdf9aa77
bySabi Files <bysabi.files@gmail.com>
2019-11-08 23:19:23 +0100

    update package.json

--- src/proxy.js
+++ src/proxy.js
@@ -1,22 +1,22 @@
-const fetch = require("node-fetch");
-const moment = require("moment");
-const invariant = require("tiny-invariant");
-const store = require("./store");
-const { translateWithProxy } = require("./puppy");
+const fetch = require('node-fetch');
+const moment = require('moment');
+const invariant = require('tiny-invariant');
+const store = require('./store');
+const { translateWithProxy } = require('./puppy');
 
 const PROXY_LIST_URL =
-  "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt";
+  'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt';
 
 let FETCHED_PROXYLIST;
 let CURRENT_PROXY;
 
 function invalidCurrentProxy() {
-  store.del("proxy.working");
+  store.del('proxy.working');
 }
 
 async function rotateProxy() {
   // last stored working proxy
-  let proxy = store.get("proxy.working");
+  let proxy = store.get('proxy.working');
   if (proxy) {
     return proxy;
   }
@@ -28,10 +28,10 @@ async function rotateProxy() {
   proxy = FETCHED_PROXYLIST.pop();
 
   // write back proxy list updated
-  store.set("proxy.fetched", FETCHED_PROXYLIST);
+  store.set('proxy.fetched', FETCHED_PROXYLIST);
   // fail is proxy list is empty
   invariant(proxy, "PROXY_LIST_IS_EMPTY!! remove '--proxy' option");
-  store.set("proxy.working", proxy);
+  store.set('proxy.working', proxy);
   return proxy;
 }
 
@@ -46,8 +46,8 @@ async function testedProxy() {
   const proxy = await rotateProxy();
   try {
     // test EN->ES traslation: "dog"->"perro"
-    const res = await translateWithProxy(["dog"], "en", "es", proxy);
-    invariant(res[0] === "perro", "TRANSLATE_USING_PROXY_FAILED");
+    const res = await translateWithProxy(['dog'], 'en', 'es', proxy);
+    invariant(res[0] === 'perro', 'TRANSLATE_USING_PROXY_FAILED');
     success = true;
   } catch (err) {
     console.error(`FAIL proxy: ${proxy} => ${err.message}`);
@@ -65,26 +65,26 @@ async function testedProxy() {
 async function proxyList() {
   const res = await fetch(PROXY_LIST_URL);
   const list = await res.text();
-  return list.split("\n").slice(0, -1);
+  return list.split('\n').slice(0, -1);
 }
 
 async function init() {
   // Last time count was updated. From `store.json`
-  let LAST_FETCHED_PROXYLIST = store.get("LAST_FETCHED_PROXYLIST");
+  let LAST_FETCHED_PROXYLIST = store.get('LAST_FETCHED_PROXYLIST');
   if (LAST_FETCHED_PROXYLIST) {
     LAST_FETCHED_PROXYLIST = moment.utc(LAST_FETCHED_PROXYLIST);
   }
 
   if (
     !LAST_FETCHED_PROXYLIST ||
-    LAST_FETCHED_PROXYLIST.diff(Date.now(), "days") !== 0 ||
-    !store.has("proxy.fetched")
+    LAST_FETCHED_PROXYLIST.diff(Date.now(), 'days') !== 0 ||
+    !store.has('proxy.fetched')
   ) {
-    store.set("LAST_FETCH_PROXYLIST", new Date().toUTCString());
     FETCHED_PROXYLIST = await proxyList();
-    store.set("proxy.fetched", FETCHED_PROXYLIST);
+    store.set('proxy.fetched', FETCHED_PROXYLIST);
+    store.set('LAST_FETCH_PROXYLIST', new Date().toUTCString());
   }
-  FETCHED_PROXYLIST = store.get("proxy.fetched");
+  FETCHED_PROXYLIST = store.get('proxy.fetched');
 }
 
 module.exports = testedProxy;
--- src/puppy.js
+++ src/puppy.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 // SEPERATOR "<*>"
 const SEPARATOR_RE = new RegExp(/\n<\*>[\n]?/);
